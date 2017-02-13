28f334642796376ba5ec765023859d945a232d07
keanemind <keanemind@gmail.com>
2017-02-13 12:13:10 -0600

    Switched to enumerate and changed the counts list

--- 1002.py
+++ 1002.py
@@ -1,11 +1,18 @@
 monsters = ("few", "several", "pack", "lots", "horde", "throng", "swarm", "zounds", "legion")
-counts = (1, 5, 10, 20, 50, 100, 250, 500, 1000)
+counts = (5, 10, 20, 50, 100, 250, 500, 1000)
 monstercount = int(input())
-for i in range(len(counts)):
-    if monstercount < counts[i]:
-        print(monsters[i-1])
-        break
-    if i == (len(counts) - 1):
-        print(monsters[i])
-        break
+# for i in range(len(counts)):
+#     if monstercount < counts[i]:
+#         print(monsters[i-1])
+#         break
+#     if i == (len(counts) - 1):
+#         print(monsters[i])
+#         break
 
+for tup in enumerate(counts):
+    if monstercount < counts[tup[0]]:
+        print(monsters[tup[0]])
+        break
+    if tup[0] == (len(counts) - 1):
+        print(monsters[len(monsters) - 1])
+        break
