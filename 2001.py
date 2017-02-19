5b75240e457f999b227422581c197b21fb257fd1
keanemind <keanemind@gmail.com>
2017-02-19 00:00:36 -0600

    Initial solution

---
+++ 2001.py
@@ -0,0 +1,4 @@
+weigh1 = [int(num) for num in input().split(" ")]
+weigh2 = [int(num) for num in input().split(" ")]
+weigh3 = [int(num) for num in input().split(" ")]
+print(weigh1[0] - weigh3[0], weigh1[1] - weigh2[1])
